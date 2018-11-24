8af4cda03b43c4c876663a599e077f56bec1539c
Kevin Baugh <kbaugh@indeed.com>
2018-11-24 15:17:11 -0600

    Initial Commit before rewriting it all

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,2 +1,39 @@
 import './silence.js';
 
+(async () => {
+  const url = window.source
+  const videos = await fetch(url).then(resp => resp.json() );
+
+
+  if(document.querySelector('json-debug')) document.querySelector('json-debug').innerHTML = `<details>
+    <summary>Current JSON Object</summary>
+    <pre><code>
+      ${JSON.stringify(videos, null, 2)}
+    </code></pre>
+  </details>`
+
+  const current_video = videos.shift()
+
+  const todaysVideo = document.createElement('todays-video')
+  todaysVideo.innerHTML = `
+    <h2>${current_video.title}</h2>
+    <p><small><i>${current_video.date}</i></small></p>
+    <iframe width="560" height="315" style="width: 560px; height: 315; max-width: 90vw" src="https://www.youtube.com/embed/${current_video.youtube_id}?controls=1"
+        frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+  `
+    document.querySelector('main').appendChild(todaysVideo)
+
+
+  
+//   for(let video of videos) {
+//       console.log(video)
+// //     if(key.match(/[a-z\-]+/i)) {
+// //       const json_elements = document.querySelectorAll(`json-${key}`)
+// //       for(let json_element of json_elements) {
+// //         buildElementFromValue(json[key], json_element)
+// //       }  
+// //     }
+//   }
+
+
+})()
--- assets/scripts/silence.js
+++ assets/scripts/silence.js
@@ -9,29 +9,28 @@ const setJsonChildren = (element, object) => {
     const child_tag = `${element_key}-${key}`
     const el = element.querySelector(child_tag);
     if(el) {
-      handleValueType(key, object[key], el);    
+      buildElementFromValue(object[key], el);    
     } else if (Array.isArray(object[key])) {
-      handleValueType(key, object, element)
+      buildElementFromValue(object, element)
     } else if(!element.firstElementChild) {
-      handleValueType(key, JSON.stringify(object, null, 2), element)
+      buildElementFromValue(JSON.stringify(object, null, 2), element)
     }
   }
 }
 
 const buildFromArray = (element, items) => {
   const template = element.querySelector('*[blueprint]')
-  console.log(template);
   for(let item of items) {
     const clone = template.cloneNode(true)
-    const textNode = document.createTextNode(item)
-    clone.appendChild(textNode)
-    element.insertBefore(clone, template)
+    // const textNode = document.createTextNode(item)
+    // clone.appendChild(textNode)
+    template.parentElement.insertBefore(clone, template)
+    buildElementFromValue(item, clone)
   }
   template.parentElement.removeChild(template)
-
 }
 
-const handleValueType = (key, value, element) => {
+const buildElementFromValue = (value, element) => {
   switch (typeof value) {
     case 'string':
     case 'number':
@@ -54,28 +53,12 @@ const handleValueType = (key, value, element) => {
   }
 }
 
-
-(async () => {
-  console.log(document.querySelector('script').getAttribute('href'))
-  const url = "https://petstore.swagger.io/v2/swagger.json"
-
-  const json = await fetch(url)
-  .then(resp => { return resp.json(url) });
-
-  for(let key in json) { 
-
-    const json_elements = document.querySelectorAll(`json-${key}`)
-    for(let json_element of json_elements) {
-      handleValueType(key, json[key], json_element)
-    }
-  }
+// const Silence = {
+//   register: (key, object) => {
+//     localStorage.setItem(`silence-${key}`, object)
+//     return this
+//   }
+// }
 
 
-  if(document.querySelector('json-debug')) document.querySelector('json-debug').innerHTML = `<details>
-    <summary>Current JSON Object</summary>
-    <pre><code>
-      ${JSON.stringify(json, null, 2)}
-    </code></pre>
-  </details>`
-})()
 
